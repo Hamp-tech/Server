cfdd2079cc7b6be721a9e764c85fdd8dcb16a407
Joan Molinas Ramon <joanmramon@gmail.com>
2017-12-08 16:17:05 +0100

    [CORE] CURL and mongo installed

--- Package.swift
+++ Package.swift
@@ -7,5 +7,8 @@ let package = Package(
     ],
     dependencies: [
         .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 3),
+                .Package(url:"https://github.com/PerfectlySoft/Perfect-MongoDB.git", versions: Version(0,0,0)..<Version(10,0,0)),
+                .Package(url: "https://github.com/PerfectlySoft/Perfect-CURL.git", majorVersion: 3)
+
         ]
 )
